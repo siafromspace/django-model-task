ff392dd976193da94783919aec9cd1d02a1e1241
siafromspace <sharonyak15@gmail.com>
2022-06-17 01:13:11 +0100

    edits

--- I4G005207BKA/blog/models.py
+++ I4G005207BKA/blog/models.py
@@ -7,6 +7,6 @@ User = get_user_model()
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    author = models.ForeignKey(get_user_model)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
