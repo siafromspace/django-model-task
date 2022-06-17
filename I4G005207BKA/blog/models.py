1a24a0d96fc76a3030e4f9991c67f179254cce02
siafromspace <sharonyak15@gmail.com>
2022-06-17 01:03:59 +0100

    created app

---
+++ I4G005207BKA/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
